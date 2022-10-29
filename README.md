9f3102dfe766027693d14dbed8086a5cc2f53c68
thisiswoo <thisiswoo0594@gmail.com>
2022-10-29 16:14:32 +0900

    add a newe to-do to the list

--- README.md
+++ README.md
@@ -3,18 +3,25 @@
 ## Project version
 
 ```
-node : 16.17.1
-vue : 5.0.8
+- node : 16.17.1
+- vue : 5.0.8
 ```
 
 ### Learning Objectives
 
 ```
-vue3와 친해지기
+- vue3와 친해지기
 ```
 
 ### Description
 
 ```
-v-model을 활용하여 양방향 바인딩을 배워보기
+- bootstrap을 이용하여 간단한 layout과 e.preventDefault를 vue문법으로 사용해보기.
+- list에 새로운 todo를 추가해보기.
+```
+
+#### Reference
+
+```
+- [Modifiers](https://vuejs.org/guide/essentials/template-syntax.html/) - prevent에 관한 설명
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,61 @@
 <template>
-  <!-- <input 
-    type="text"
-    :value="name"
-    @input="updateName"
-  > -->
-  <!-- 
-    v-model을 사용하면 주석한 내용의 코드를 간편하게 줄일 수 있다.
-  -->
-  <input 
-    type="text"
-    v-model="name"
-  >
-  <button 
-    class="btn btn-primary" 
-    @click="onSubmit"
-  >
-    Click
-  </button>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <!--
+      @submit.prevent="xxx" 를 사용하여 간략하게 표현 가능.
+      <form 
+        @submit="onSubmit"
+        class="d-flex"
+      > 
+    -->
+    <form 
+      @submit.prevent="onSubmit"
+      class="d-flex"
+    >
+      <div class="flex-grow-1 mr-2">
+        <input 
+          class="form-control"
+          type="text"
+          v-model="todo"
+          placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary" 
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    {{ todos }}
+  </div>
 </template>
 
 <script>
-import { ref } from "vue"; // number, string
+import { ref } from "vue"; 
 
 export default {
   name: "App",
   setup() {
-    const name = ref('this is'); // number, string
+    const todo = ref(''); 
+    const todos = ref([]);  // arry
     
+    // @submit.prevent="xxx" 를 사용하면 코드를 간결하게 줄일 수 있다.
+    // const onSubmit = (e) => {
     const onSubmit = () => {
-      console.log(name.value);
+      // e.preventDefault();
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+      });
     };
 
-    // const updateName = (e) => {
-    //   console.log(e.target.value);
-    //   console.log(e);
-    //   name.value = e.target.value;
-    // };
-
     return {
-      name,
+      todo,
+      todos,
       onSubmit,
-      // updateName,
     };
   },
 };

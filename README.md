aaad11b52e2e673d8d9af97a44ac263a414c74b8
thisiswoo <thisiswoo0594@gmail.com>
2022-10-31 18:07:33 +0900

    learn the difference between v-show and v-if

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Project version
 
 - node : 16.17.1
-- vue : 5.0.8
+- vue : 3.2.41
 
 ### Learning Objectives
 
@@ -11,4 +11,9 @@
 
 ### Description
 
-- bootstrap을 이용하여 간단한 to-do 카드 만들어 보기
+- v-show와 v-if의 사용과 차이점 알아보기.
+- [v-show VS v-if](https://vuejs.org/guide/essentials/conditional.html)
+- v-show : 초기 렌더링 비용이 더 높다. 실제(real) 조건부 렌더링이기 때문에 초기 렌더링 시 조건이 거짓(false)면 아무 작업도 하지 않고, 참(true)가 될 때까지 렌더링 되지 않는다.
+- v-if : 토글 비용이 더 높다. v-if, v-else-if 또는 v-else를 모두 렌더링 해야 하기 때문에 초기 렌더링 비용이 더 높다. 렌더링 된 후 조건에 따라 css속성이 display: block 또는 display: none으로 속성을 전환한다.
+
+- **결론** toggle 같은 기능을 자주 사용하는 경우 `v-show`를, runtime 동안 조건이 거의 바뀌지 않을때는 `v-if`를
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
+  <!-- 
+    v-show : 초기 브라우저가 랜더링 될 때
+            onst toggle = ref(false) <-- false로 지정해줘서
+            태그 안에 style display:none으로 지정되어 false인 div만 출력 됨.
+            * 초기 렌더링 비용이 많이 소모됨.
+            이유 : 지금 같이 if,else의 div를 모두 렌더링 해야 하므로...
+            *** toggle 같은 기능을 자주 사용하는 경우 v-show를 사용해야 함.
+
+    <div v-show="toggle">true</div>
+    <div v-show="!toggle">false</div> 
+  -->
+
+  <!-- 
+    v-if : 초기 브라우저 렌더링 될 때 
+          const toggle = ref(false) <-- false로 지정해줘서
+          초기 값이 false인 div가 노출 됨.
+          * toggle 하는데 비용이 많이 들어감.
+          *** runtime 동안 조건이 거의 바뀌지 않을때 사용해야 함.
+  -->
+  <div v-if="toggle">true</div>
+  <div v-else>false</div>
+
+  <button @click="onToggle">Toggle</button>
+
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit" class="d-flex">
-      <div class="flex-grow-1 mr-2">
-        <input
-          class="form-control"
-          type="text"
-          v-model="todo"
-          placeholder="Type new to-do"
-        />
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new to-do"
+          />
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">Add</button>
+        </div>
       </div>
-      <div>
-        <button class="btn btn-primary" type="submit">Add</button>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
       </div>
     </form>
     <!-- {{ todos }} -->
@@ -36,6 +65,7 @@ import { ref } from "vue";
 export default {
   name: "App",
   setup() {
+    const toggle = ref(false);
     const todo = ref("");
     const todos = ref([
       {
@@ -47,18 +77,32 @@ export default {
         subject: "삼겹살 사기",
       },
     ]);
+    const hasError = ref(false);
 
     const onSubmit = () => {
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value,
-      });
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+        });
+        hasError.value = false;
+      }
+    };
+
+    // v-show
+    const onToggle = () => {
+      toggle.value = !toggle.value;
     };
 
     return {
       todo,
       todos,
       onSubmit,
+      toggle,
+      onToggle,
+      hasError,
     };
   },
 };

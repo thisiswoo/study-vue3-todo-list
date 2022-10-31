daf1566cc1d96468a8289dd5536a59e17610d01d
thisiswoo <thisiswoo0594@gmail.com>
2022-10-31 15:27:07 +0900

    using v-for

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,52 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form 
-      @submit.prevent="onSubmit"
-      class="d-flex"
-    >
+    <form @submit.prevent="onSubmit" class="d-flex">
       <div class="flex-grow-1 mr-2">
-        <input 
+        <input
           class="form-control"
           type="text"
           v-model="todo"
           placeholder="Type new to-do"
-        >
+        />
       </div>
       <div>
-        <button 
-          class="btn btn-primary" 
-          type="submit"
-        >
-          Add
-        </button>
+        <button class="btn btn-primary" type="submit">Add</button>
       </div>
     </form>
-    {{ todos }}
-    <div class="card mt-2">
+    <!-- {{ todos }} -->
+    <div v-for="t in todos" :key="t.id" class="card mt-2">
       <div class="card-body p-2">
-        {{ todos[0].subject }}
+        {{ t.subject }}
       </div>
     </div>
-    <div class="card mt-2">
-      <div class="card-body p-2">
-        {{ todos[1].subject }}
-      </div>
-    </div>
+    <!-- 
+      <div class="card mt-2">
+        <div class="card-body p-2">
+          {{ todos[1].subject }}
+        </div>
+      </div> 
+    -->
   </div>
 </template>
 
 <script>
-import { ref } from "vue"; 
+import { ref } from "vue";
 
 export default {
   name: "App",
   setup() {
-    const todo = ref(''); 
+    const todo = ref("");
     const todos = ref([
       {
-        id: 1, 
-        subject: '휴대폰 사기'
+        id: 1,
+        subject: "휴대폰 사기",
       },
       {
         id: 2,
-        subject: '삼겹살 사기'
-      }
-    ]); 
+        subject: "삼겹살 사기",
+      },
+    ]);
 
     const onSubmit = () => {
       todos.value.push({
@@ -71,5 +65,7 @@ export default {
 </script>
 
 <style>
-  .name { color: #ff8080; }
+.name {
+  color: #ff8080;
+}
 </style>

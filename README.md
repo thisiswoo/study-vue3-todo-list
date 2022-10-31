5734b88ea8da382fba8a7a0c38365b2860e92dde
thisiswoo <thisiswoo0594@gmail.com>
2022-10-31 18:31:44 +0900

    learn about checkbox bindings

--- README.md
+++ README.md
@@ -11,9 +11,4 @@
 
 ### Description
 
-- v-show와 v-if의 사용과 차이점 알아보기.
-- [v-show VS v-if vue.js 공식문서 참고](https://vuejs.org/guide/essentials/conditional.html)
-- v-show : 초기 렌더링 비용이 더 높다. 실제(real) 조건부 렌더링이기 때문에 초기 렌더링 시 조건이 거짓(false)면 아무 작업도 하지 않고, 참(true)가 될 때까지 렌더링 되지 않는다.
-- v-if : 토글 비용이 더 높다. v-if, v-else-if 또는 v-else를 모두 렌더링 해야 하기 때문에 초기 렌더링 비용이 더 높다. 렌더링 된 후 조건에 따라 css속성이 display: block 또는 display: none으로 속성을 전환한다.
-
-- **결론** toggle 같은 기능을 자주 사용하는 경우 `v-show`를, runtime 동안 조건이 거의 바뀌지 않을때는 `v-if`를
+- input checkbox 바인딩에 대해 알아보기.
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,4 @@
 <template>
-  <!-- 
-    v-show : 초기 브라우저가 랜더링 될 때
-            onst toggle = ref(false) <-- false로 지정해줘서
-            태그 안에 style display:none으로 지정되어 false인 div만 출력 됨.
-            * 초기 렌더링 비용이 많이 소모됨.
-            이유 : 지금 같이 if,else의 div를 모두 렌더링 해야 하므로...
-            *** toggle 같은 기능을 자주 사용하는 경우 v-show를 사용해야 함.
-
-    <div v-show="toggle">true</div>
-    <div v-show="!toggle">false</div> 
-  -->
-
-  <!-- 
-    v-if : 초기 브라우저 렌더링 될 때 
-          const toggle = ref(false) <-- false로 지정해줘서
-          초기 값이 false인 div가 노출 됨.
-          * toggle 하는데 비용이 많이 들어감.
-          *** runtime 동안 조건이 거의 바뀌지 않을때 사용해야 함.
-  -->
-  <div v-if="toggle">true</div>
-  <div v-else>false</div>
-
-  <button @click="onToggle">Toggle</button>
-
   <div class="container">
     <h2>To-Do List</h2>
     <form @submit.prevent="onSubmit">
@@ -43,19 +19,24 @@
         This field cannot be empty
       </div>
     </form>
-    <!-- {{ todos }} -->
-    <div v-for="t in todos" :key="t.id" class="card mt-2">
+    <div 
+      v-for="t in todos" 
+      :key="t.id" 
+      class="card mt-2"
+    >
       <div class="card-body p-2">
-        {{ t.subject }}
+        <div class="form-check">
+          <input 
+            class="form-check-input" 
+            type="checkbox"
+            v-model="t.completed"
+          >
+          <label class="form-check-label" >
+            {{ t.subject }}
+          </label>
+        </div>
       </div>
     </div>
-    <!-- 
-      <div class="card mt-2">
-        <div class="card-body p-2">
-          {{ todos[1].subject }}
-        </div>
-      </div> 
-    -->
   </div>
 </template>
 
@@ -65,18 +46,8 @@ import { ref } from "vue";
 export default {
   name: "App",
   setup() {
-    const toggle = ref(false);
     const todo = ref("");
-    const todos = ref([
-      {
-        id: 1,
-        subject: "휴대폰 사기",
-      },
-      {
-        id: 2,
-        subject: "삼겹살 사기",
-      },
-    ]);
+    const todos = ref([]);
     const hasError = ref(false);
 
     const onSubmit = () => {
@@ -86,22 +57,18 @@ export default {
         todos.value.push({
           id: Date.now(),
           subject: todo.value,
+          completed: false,
+          // completed: true,    // 차이점 확인. checkbox에 자동으로 체크됨.
         });
         hasError.value = false;
+        todo.value = '';        // input 필드 작성 후 add 버튼 누른 후 input 안에 empty 만들어 주기.
       }
-    };
-
-    // v-show
-    const onToggle = () => {
-      toggle.value = !toggle.value;
     };
 
     return {
       todo,
       todos,
       onSubmit,
-      toggle,
-      onToggle,
       hasError,
     };
   },

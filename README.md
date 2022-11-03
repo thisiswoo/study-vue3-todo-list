43b7ff8d36aecd909661b360987f5761fe481867
thisiswoo <thisiswoo0594@gmail.com>
2022-11-03 10:13:14 +0900

    make form tag components

--- README.md
+++ README.md
@@ -11,8 +11,4 @@
 
 ### Description
 
-- input checkbox 바인딩에 대해 알아보기.
-
-### Reference
-
-- `v-for="(t, index) in todos" :key="t.id"`의 배열에 `index`를 추가하여 추가된 `todo list` 삭제하기.
+- form 태그의 정보를 components로 만들기.
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control"
-            type="text"
-            v-model="todo"
-            placeholder="Type new to-do"
-          />
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">Add</button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red">
-        This field cannot be empty
-      </div>
-    </form>
+    <TodoSimpleForm />
     <div v-if="!todos.length">
       추가된 Todo가 없습니다.
     </div>
@@ -56,9 +39,13 @@
 
 <script>
 import { ref } from "vue";
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
   name: "App",
+  components: {
+    TodoSimpleForm
+  },
   setup() {
     const todo = ref("");
     const todos = ref([]);
